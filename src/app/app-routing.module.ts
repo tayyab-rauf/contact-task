8c7d74a07ead5062e232cbb04acc57c57e675ef7
Tayyab <tayyab.rauf@gmail.com>
2024-01-25 19:09:46 +0500

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/contact-list',
-  },
-  {
-    path: 'contact-list',
-    component: ContactListComponent,
-  },
-  {
-    path: 'contact-details',
-    component: ContactDetailsComponent,
-  },
- 
+const routes: Routes = [ 
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ContactDetailsComponent } from './contact-details/contact-details.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactDetailsComponent,
-    ContactListComponent
   ],
   imports: [
     BrowserModule,
